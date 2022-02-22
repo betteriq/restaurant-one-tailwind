6e50d5c34bfa1e7ac31f05f1b250e1a48fa8f9dd
betteriq <anoushrs@gmail.com>
2022-02-22 14:41:02 -0800

    about-f

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,12 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="navbar_text">
-        <div className="logo">
+    <nav className="absolute top-0 left-0 z-10 w-full">
+      <div className="flex h-16 items-center my-0 mx-5 w-[1200]">
+        <div className="w-2/3 text-aliceblue">
           <h4>Rashidi</h4>
         </div>
-        <ul className="navbar-ul">
+        <ul className="h-3/5 list-none flex justify-end items-center navbar_ul">
           <li>
             <a href="">خانه</a>
           </li>
